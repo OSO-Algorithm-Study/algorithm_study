4dfd5198f822f8a3dd5805ed645cca25fedac02d
kimseungdeok <tmdejr1117@gmail.com>
2022-10-31 19:39:12 +0900

    resolved : 1449 풀이 완료

--- kimseungdeok/1449.py
+++ kimseungdeok/1449.py
@@ -6,15 +6,19 @@ input = stdin.readline
 n, l = map(int, input().split())
 
 locations = list(map(int,list(input().split(' '))))
-answer = 0
-
 locations.sort()
 
-temp = 0
-for i in range(1,n):
-    if locations[i]-locations[temp] > l:
-        temp += 1
-        print(temp)
-        answer += 1
+tape = []
+tape.append(locations[0] - 0.5)
+tape.append(tape[0] + l)
+count = 1
 
-print(answer)
+for i in range(1,n):
+    if tape[0] < locations[i] < tape[1]:
+        continue
+    else:
+        tape[0] = locations[i] - 0.5
+        tape[1] = tape[0] + l
+        count += 1
+
+print(count)
