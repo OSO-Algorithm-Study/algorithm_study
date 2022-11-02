682b78001c0d584e41438c67d9e3d1a6d4cd0908
kimseungdeok <tmdejr1117@gmail.com>
2022-11-02 16:06:37 +0900

    resolved : 20221102 18310 풀이 완료

--- kimseungdeok/18310.py
+++ kimseungdeok/18310.py
@@ -1,30 +1,9 @@
 from sys import stdin
 
-stdin = open('example.txt', 'r')
 input = stdin.readline
 
 n = int(input().strip())
 houses = list(map(int, input().split()))
-
 houses.sort()
 
-def sumDiff(start):
-    sum = 0
-    for i in range(n-start):
-        sum += abs(houses[start]-houses[i])
-    return (start,sum)
-
-# print(sumDiff(int(n/2)))
-
-answer = []
-if n == 1 or n == 2:
-    print(houses[0])
-else:
-    if n % 2 == 1:
-        print(houses[int(n/2)])
-    else:
-        answer.append(sumDiff(int(n/2)))
-        answer.append(sumDiff(int(n/2)+1))
-        answer.sort(key=lambda x:x[1])
-        print(houses[answer[0][0]])
-
+print(houses[(n-1)//2])
