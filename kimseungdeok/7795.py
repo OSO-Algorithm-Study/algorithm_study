02c26de191ffb0b02906a327634f698fcecf9afa
kimseungdeok <tmdejr1117@gmail.com>
2022-11-05 15:17:58 +0900

    resolved : 7795 풀이 완료

--- kimseungdeok/7795.py
+++ kimseungdeok/7795.py
@@ -1,30 +1,37 @@
 from sys import stdin
 
+# 2
+# 5 3
+# 8 1 7 3 1
+# 3 6 1
+# 3 4
+# 2 13 7
+# 103 11 290 215
+
 stdin = open('example.txt', 'r')
 input = stdin.readline
 
 tc = int((input())) # 테스트 케이스 개수
 
 for _ in range(tc):
-    n, m = map(int, input().split())
+    n, m = map(int, input().split()) # a 마리수 n, b 마리수 m
     nList = list(map(int, input().split()))
     mList = list(map(int, input().split()))
     nList.sort()
     mList.sort()
 
-    nNum = 0
-    mNum = 0 # 투포인터를 위해 만든 변수들
     count = 0
+    pair = 0
 
-    while True:
-        if nNum > n or mNum > m:
-            break
-        if nList[nNum] <= mList[mNum]:
-            nNum += 1
-        elif nList[nNum] > mList[mNum]:
-            count += 1
-            mNum += 1
-    print(count)
+    for i in range(n):
+        while True:
+            if count == m or nList[i] <= mList[count]:
+                pair += count
+                break
+            else:
+                count += 1
+
+    print(pair)
 
 
 
