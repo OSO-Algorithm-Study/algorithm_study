246d5d9302cb0f9561c5bffb98d049f133d4b1af
jamiehun <jamiesunghun@gmail.com>
2022-11-08 14:30:03 +0900

    correct:16953

--- 문제출제/완전탐색/20221108_bj_16953.py
+++ 문제출제/완전탐색/20221108_bj_16953.py
@@ -1,2 +1,31 @@
 # https://www.acmicpc.net/problem/16953
 # 16953번: A -> B
+
+a, b = map(int, input().split())
+count = 0
+count_list = []
+min_count = 1e9
+
+def dfs(a, target, count):
+    global min_count
+    
+    if a > target:
+        return
+    
+    elif a == target and count < min_count:
+        min_count = min(min_count, count)
+        count_list.append(count)
+        return 
+    
+    else:
+        dfs((a * 2), target, count + 1)
+        dfs(int((str(a) + str(1))), target, count + 1)
+        return
+
+dfs(a, b, 0)
+# print(count_list)
+if count_list:
+    print(min(count_list) + 1)
+else:
+    print(-1)
+
