2475f9ce819eb51596fd440543e863129857f117
Songhee Lee <shine.p715@gmail.com>
2022-11-09 15:32:03 +0900

    221109

---
+++ SongheeLee/1764.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+dict=[]
+never_heard=set()
+never_seen=set()
+
+for i in range(N):
+    never_heard.add(input().strip())
+
+for i in range(M):
+    never_seen.add(input().strip())
+
+never_heard_seen = list(never_seen & never_heard)
+
+never_heard_seen.sort()
+print(never_heard_seen)
