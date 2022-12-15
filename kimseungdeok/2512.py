37ee88d02c26d96c959668d4ca4b36348c2d6a78
kimseungdeok <tmdejr1117@gmail.com>
2022-12-15 11:25:30 +0900

    resolved : 2512 풀이 완료

--- kimseungdeok/2512.py
+++ kimseungdeok/2512.py
@@ -8,8 +8,20 @@ arr = list(map(int,input().split())) # 각 지방 예산
 arr.sort()
 M = int(input()) # 총 예산
 
-# if(sum(arr) <= M):
-#     print(max(arr))
-# else:
+start, end = 0, max(arr)
+
+
+while start <= end:
+    mid = (start + end)//2
+    num = 0
+    for i in arr:
+        if i >= mid:
+            num += mid
+        else:
+            num += i
+    if num <= M:
+        start = mid +1
+    else:
+        end = mid-1
+print(end)
     
-print(120+110+140+150)
