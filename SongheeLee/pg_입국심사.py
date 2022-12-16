4fd58cf0cba94c88aee07042c53107d1f4ec0b53
Songhee Lee <shine.p715@gmail.com>
2022-12-16 12:55:28 +0900

    프로그래머스 - 입국 심사

---
+++ SongheeLee/pg_입국심사.py
@@ -0,0 +1,34 @@
+import sys
+
+n = 6
+times = [7, 10]
+
+# 코드 시작
+desk = len(times)
+
+if n%desk == 0:
+    m = n/desk
+else:
+    m = n/desk + 1
+
+times.sort()
+
+start = times[0] * m
+end = times[-1] * m
+
+while start <= end:
+    mid = (start + end) // 2
+    num = 0
+
+    for time in times:
+        num += (mid // time)
+
+    if num < n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+answer = start
+# 코드 끝
+    
+print(answer)
