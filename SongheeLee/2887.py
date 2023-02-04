72567d2a3f6c12d71825eb8b9cbc84b98155310d
Songhee Lee <shine.p715@gmail.com>
2023-02-04 16:56:43 +0900

    pass

--- SongheeLee/2887.py
+++ SongheeLee/2887.py
@@ -1,12 +1,9 @@
-from itertools import combinations
-
 # 특정 원소가 속한 집합을 찾기
 def find(parent, x):
     if parent[x] == x:
         return x
     parent[x] = find(parent, parent[x])
     return parent[x]
-
 
 # 두 원소가 속한 집합을 합치기 (간선 연결한다고 생각!)
 def union(parent, a, b):
@@ -18,35 +15,40 @@ def union(parent, a, b):
     else:
         parent[rootA] = rootB
 
-
 import sys
 
 input = sys.stdin.readline
 # 노드의 개수와 간선(union 연산)의 개수 입력받기
 n = int(input())
-position = []
-for _ in range(n):
+positionX = []
+positionY = []
+positionZ = []
+for i in range(n):
     x, y, z = map(int, input().split())
-    position.append((x,y,z))
+    positionX.append((x, i))
+    positionY.append((y, i))
+    positionZ.append((z, i))
+
+positionX.sort()
+positionY.sort()
+positionZ.sort()
 
 parent = [0] * n
 # 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(n):
     parent[i] = i
 
-lines = list(combinations(parent,2))
-
 edges = []
 
-# 모든 간선에 대한 정보를 입력받기
-for line in lines:
-    a, b = line
-    cost = min(abs(position[a][0] - position[b][0]),
-    abs(position[a][1] - position[b][1]),
-    abs(position[a][2] - position[b][2]))
-
-    # 비용순으로 오름차순 정렬하기 위해 튜플의 첫 번째 원소를 비용으로 설정
-    edges.append((cost, a, b))
+for i in range(n-1):
+    cost = abs(positionX[i][0] - positionX[i+1][0])
+    edges.append((cost, positionX[i][1], positionX[i+1][1]))
+   
+    cost = abs(positionY[i][0] - positionY[i+1][0])
+    edges.append((cost, positionY[i][1], positionY[i+1][1]))
+   
+    cost = abs(positionZ[i][0] - positionZ[i+1][0])
+    edges.append((cost, positionZ[i][1], positionZ[i+1][1]))
 
 result = 0
 edges.sort()
