a0e7c2f1b10c3e2af7855bf0d8fd4d1233f02039
YoungwooKim09 <youngwoo9699@gmail.com>
2023-02-06 04:25:02 +0900

    resolvd: BJ_14226_이모티콘

--- YoungwooKim/2512.py
+++ YoungwooKim/2512.py
@@ -11,12 +11,14 @@ budget_list.sort()
 
 tot = int(input())
 
+# 모든 요청이 배정될 수 있는 경우
 if tot >= sum(budget_list):
     print(budget_list[-1])
 
+# 모든 요청이 상한액으로 배정되어야 할 경우
 elif tot <= budget_list[0] * N:
     print(tot // N)
-    
+
 else:
     start = budget_list[0]
     end = budget_list[-1]
@@ -24,16 +26,23 @@ else:
     while(start <= end):
         res = 0
         mid = (start + end) // 2
-        if start == mid:
-            break
+        
         for budget in budget_list:
-            if budget <= mid:
+            if (budget <= mid):
                 res += budget
             else:
                 res += mid
-        if res > tot:
-            end = mid
+        if (res > tot):
+            end = mid - 1
         else:
-            start = mid
+            start = mid + 1
 
-    print(mid)
+    print(end)
+
+    #         if (res > tot):
+    #         end = mid - 1
+    #     else:
+    #         ans = mid
+    #         start = mid + 1
+
+    # print(ans)
