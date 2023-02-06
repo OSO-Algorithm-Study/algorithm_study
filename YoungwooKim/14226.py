6185427ef804a3617233f6ca359100e565a2a444
YoungwooKim09 <youngwoo9699@gmail.com>
2023-02-07 00:04:35 +0900

    resolved: BJ_2343_BinarySearch_기타레슨

--- YoungwooKim/14226.py
+++ YoungwooKim/14226.py
@@ -10,45 +10,45 @@ s = int(input())
 q = deque()
 q.append((1, 0))
 
-visited = [[-1] * (s + 1) for _ in range(s + 1)]
-visited[1][0] = 0
+# visited = [[-1] * (s + 1) for _ in range(s + 1)]
+# visited[1][0] = 0
 
-# visited = dict()
-# visited[(1, 0)] = 0
+visited = dict()
+visited[(1, 0)] = 0
 
 while q:
     emote, clip = q.popleft()
-#     if (emote == s):
-#         print(visited[(emote, clip)])
-#         break
-
-#     if (emote, emote) not in visited.keys():
-#         visited[(emote, emote)] = visited[(emote, clip)] + 1
-#         q.append((emote, emote))
-
-#     if (emote + clip, clip) not in visited.keys():
-#         visited[(emote + clip, clip)] = visited[(emote, clip)] + 1
-#         q.append((emote + clip, clip))
-
-#     if (emote - 1, clip) not in visited.keys():
-#         visited[(emote - 1, clip)] = visited[(emote, clip)] + 1
-#         q.append((emote - 1, clip))
-
     if (emote == s):
-        print(visited[emote][clip])
+        print(visited[(emote, clip)])
         break
 
-    if (visited[emote][emote] == -1):
-        visited[emote][emote] = visited[emote][clip] + 1
+    if (emote, emote) not in visited.keys():
+        visited[(emote, emote)] = visited[(emote, clip)] + 1
         q.append((emote, emote))
 
-    if (emote + clip) <= s and (visited[emote + clip][clip] == -1):
-        visited[emote + clip][clip] = visited[emote][clip] + 1
+    if (emote + clip, clip) not in visited.keys():
+        visited[(emote + clip, clip)] = visited[(emote, clip)] + 1
         q.append((emote + clip, clip))
 
-    if (emote - 1) >= 0 and (visited[emote - 1][clip] == -1):
-        visited[emote - 1][clip] = visited[emote][clip] + 1
+    if (emote - 1) >= 0 and (emote - 1, clip) not in visited.keys():
+        visited[(emote - 1, clip)] = visited[(emote, clip)] + 1
         q.append((emote - 1, clip))
+
+    # if (emote == s):
+    #     print(visited[emote][clip])
+    #     break
+
+    # if (visited[emote][emote] == -1):
+    #     visited[emote][emote] = visited[emote][clip] + 1
+    #     q.append((emote, emote))
+
+    # if (emote + clip) <= s and (visited[emote + clip][clip] == -1):
+    #     visited[emote + clip][clip] = visited[emote][clip] + 1
+    #     q.append((emote + clip, clip))
+
+    # if (emote - 1) >= 0 and (visited[emote - 1][clip] == -1):
+    #     visited[emote - 1][clip] = visited[emote][clip] + 1
+    #     q.append((emote - 1, clip))
 
 # res = 10e9
 
