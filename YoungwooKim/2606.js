87b7c81bc836d82b15d05e459595deb5ebd178e1
YoungwooKim09 <youngwoo9699@gmail.com>
2023-02-15 16:28:02 +0900

    Feat: 230215_pg_타겟넘버_DFS

---
+++ YoungwooKim/2606.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const solution = (graph) => {
+  let queue = [1];
+  let count = 0;
+  while (queue.length) {
+    let v = queue.shift();
+    if (visited[v]) continue;
+    visited[v] = true;
+    count++;
+    graph[v].forEach((e) => {
+      if (!queue.includes(e)) queue.push(e);
+    });
+  }
+  console.log(count - 1);
+};
+
+const N = parseInt(input.shift());
+const M = parseInt(input.shift());
+const visited = Array.from({ length: N + 1 }, () => false);
+const graph = Array.from({ length: N + 1 }, () => []);
+// const visited = Array(N + 1).fill(false);
+// const graph = Array(N + 1).fill([]);
+
+for (let i = 0; i < M; i++) {
+  [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+}
+
+solution(graph);
