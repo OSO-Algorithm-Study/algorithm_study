b429f5481b5a96371d10caae2cb33438e4217a87
jamiehun <jamiesunghun@gmail.com>
2023-02-15 14:20:44 +0900

    fix:folder-sorting

---
+++ 2월3주차(BFS,DFS)/230215_pg_타겟넘버_sh.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def solution(numbers, target):
+    result = bfs(numbers, target)
+    print(result)
+    
+    return result
+    
+def bfs(numbers, target):
+    result = 0
+    idx = 0
+    size = len(numbers)
+    temp = numbers[idx]
+    q = deque([])
+    q.append((idx, temp))
+    q.append((idx, -temp))
+    
+    while q:
+        comb = q.popleft()
+        idx = comb[0]
+        number = comb[1]
+        
+        if idx == (size-1):
+            if number == target:
+                result += 1
+            continue
+        
+        temp = numbers[idx+1]
+        
+        q.append((idx+1, number+temp))
+        q.append((idx+1, number-temp))
+        
+    return result
