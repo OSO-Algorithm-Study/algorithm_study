8398bfef2e57f639caa0a36910d41e6f31b9b2ec
YoungwooKim09 <youngwoo9699@gmail.com>
2023-02-17 20:34:58 +0900

    resolved

---
+++ 2월3주차(BFS,DFS)/230217_pg_네트워크_yw.js
@@ -0,0 +1,84 @@
+// BFS
+function solution(n, computers) {
+  let answer = 0;
+  let visited = Array.from({ length: n }, () => false);
+
+  // BFS
+  function bfs(x) {
+    let queue = [x];
+    visited[x] = true;
+
+    while (queue.length) {
+      let c = queue.shift();
+      for (let i = 0; i < n; i++) {
+        if (visited[i] == false && computers[c][i] == 1) {
+          visited[i] = true;
+          queue.push(i);
+        }
+      }
+    }
+  }
+  for (let j = 0; j < n; j++) {
+    if (visited[j] == false) {
+      bfs(j);
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
+
+// DFS - Recursion
+function solution(n, computers) {
+  let answer = 0;
+  let visited = Array.from({ length: n }, () => false);
+
+  function dfs(x) {
+    visited[x] = true;
+
+    for (let i = 0; i < n; i++) {
+      if ((visited[i] == false) & (computers[x][i] == 1)) {
+        dfs(i);
+      }
+    }
+  }
+
+  for (let j = 0; j < n; j++) {
+    if (visited[j] == false) {
+      dfs(j);
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
+
+// DFS - Stack
+function solution(n, computers) {
+  let answer = 0;
+  let visited = Array.from({ length: n }, () => false);
+
+  function dfs(x) {
+    let stack = [x];
+    visited[x] = true;
+
+    while (stack.length) {
+      k = stack.pop();
+      for (let i = 0; i < n; i++) {
+        if (visited[i] == false && computers[k][i] == 1) {
+          visited[i] = true;
+          stack.push(i);
+        }
+      }
+    }
+  }
+
+  for (let j = 0; j < n; j++) {
+    if (visited[j] == false) {
+      dfs(j);
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
