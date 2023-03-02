1d203cbaf547fc915b7729feb42d9e12dd194e4e
young970 <yyww9@naver.com>
2023-03-02 23:49:39 +0900

    Fix: 연결리스트 큐 toArray메서드 추가

--- 3월/연결리스트_큐.js
+++ 3월/연결리스트_큐.js
@@ -1,4 +1,4 @@
-// 노드 클래스를 정의합니다.
+// 연결리스트의 노드 클래스 정의.
 class Node {
   constructor(data) {
     this.data = data;
@@ -6,36 +6,37 @@ class Node {
   }
 }
 
-// 큐 클래스를 정의합니다.
-class Queue {
+// 큐 클래스 정의.
+class queue {
   constructor() {
-    this.front = null;
-    this.rear = null;
+    this.head = null;
+    this.tail = null;
     this.size = 0;
   }
 
-  // 큐에 데이터를 추가합니다.
+  //큐에 데이터 추가.
   enqueue(data) {
     const node = new Node(data);
-    if (this.rear === null) {
-      this.front = node;
-      this.rear = node;
+
+    if (this.tail === null) {
+      this.head = node;
+      this.tail = node;
     } else {
-      this.rear.next = node;
-      this.rear = node;
+      this.tail.next = node;
+      this.tail = node;
     }
     this.size++;
   }
 
-  // 큐에서 데이터를 제거하고 반환합니다.
+  // 큐에 데이터 제거하고 반환.
   dequeue() {
-    if (this.front === null) {
+    if (this.head === null) {
       return null;
     }
-    const data = this.front.data;
-    this.front = this.front.next;
-    if (this.front === null) {
-      this.rear = null;
+    const data = this.head.data;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
     }
     this.size--;
     return data;
@@ -43,16 +44,28 @@ class Queue {
 
   // 큐의 맨 앞에 있는 데이터를 반환합니다.
   peek() {
-    return this.front ? this.front.data : null;
+    return this.head ? this.head.data : null;
   }
 
-  // 큐가 비어 있는지 확인합니다.
+  // 큐가 비어 있는지 확인.
   isEmpty() {
     return this.size === 0;
   }
 
-  // 큐의 크기를 반환합니다.
+  // 큐의 크기 반환.
   getSize() {
     return this.size;
+  }
+
+  // 큐의 모든 리스트 배열로 반환.
+  toArray() {
+    const result = [];
+
+    let current = this.head;
+    while (current) {
+      result.push(current.data);
+      current = current.next;
+    }
+    return result;
   }
 }
